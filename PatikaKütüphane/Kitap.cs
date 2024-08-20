de67c524b729b818a44ae03886f10c9e808a42dc
ericekirem <irem.ericek@icloud.com>
2024-08-20 11:01:43 +0300

    Class ve Program.cs dosyalaması yapıldı

---
+++ PatikaKütüphane/Kitap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKütüphane
+{
+    public class Kitap
+    {
+        // Property'ler
+        public string Ad { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        // Default Constructor
+        public Kitap()
+        {
+            KayitTarihi = DateTime.Now;
+        }
+
+        // Parametreli Constructor
+        public Kitap(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+        {
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinevi;
+            KayitTarihi = DateTime.Now;
+        }
+
+        // Kitap bilgilerini yazdırmak için bir metot
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Ad: {Ad}, Yazar: {YazarAdi} {YazarSoyadi}, Sayfa Sayısı: {SayfaSayisi}, Yayınevi: {Yayinevi}, Kayıt Tarihi: {KayitTarihi}");
+        }
+    }
+}
--- PatikaKütüphane/Program.cs
+++ PatikaKütüphane/Program.cs
@@ -1,44 +1,11 @@
-﻿using System;
+﻿using PatikaKütüphane;
+using System;
 
 namespace PatikaKutuphane
 {
-    public class Kitap
-    {
-        // Property'ler
-        public string Ad { get; set; }
-        public string YazarAdi { get; set; }
-        public string YazarSoyadi { get; set; }
-        public int SayfaSayisi { get; set; }
-        public string Yayinevi { get; set; }
-        public DateTime KayitTarihi { get; set; }
-
-        // Default Constructor
-        public Kitap()
-        {
-            KayitTarihi = DateTime.Now;
-        }
-
-        // Parametreli Constructor
-        public Kitap(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
-        {
-            Ad = ad;
-            YazarAdi = yazarAdi;
-            YazarSoyadi = yazarSoyadi;
-            SayfaSayisi = sayfaSayisi;
-            Yayinevi = yayinevi;
-            KayitTarihi = DateTime.Now;
-        }
-
-        // Kitap bilgilerini yazdırmak için bir metot
-        public void PrintInfo()
-        {
-            Console.WriteLine($"Ad: {Ad}, Yazar: {YazarAdi} {YazarSoyadi}, Sayfa Sayısı: {SayfaSayisi}, Yayınevi: {Yayinevi}, Kayıt Tarihi: {KayitTarihi}");
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Default constructor ile kitap nesnesi oluşturma
             Kitap kitap1 = new Kitap();
